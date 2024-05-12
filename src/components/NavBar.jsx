78bf60c6c0f604d8c47f178d13705183958c14bd
josuemj <josuemj456@gmail.com>
2024-05-11 20:07:56 -0600

    side sketched

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import './NavBar.css'
+function NavBar(){
+    return(
+        <>
+        <div className="navBar">
+            <ul>
+                <li>
+                    <a>About me</a>
+                </li>
+                <li>
+                    <a>Skills</a>
+                </li>
+                <li>
+                    <a>Proyects</a>
+                </li>
+                <li>
+                    <a>Sport</a>
+                </li>
+            </ul>
+
+        </div>
+        </>
+    )
+}
+
+export default NavBar
