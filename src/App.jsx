24626adb2fd259a53980fb25f317e8220e70a9e1
josuemj <josuemj456@gmail.com>
2024-05-22 14:17:54 -0600

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { GiHamburgerMenu } from "react-icons/gi";
 import About from "./components/about/About";
 
 function App() {
-  const [showNav, setShowNav] = useState(false)
+  const [showNav, setShowNav] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [hideNavBar, setHideNavBar] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY) {
+        // Scrolling down
+        setHideNavBar(true);
+      } else {
+        // Scrolling up
+        setHideNavBar(false);
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
 
   return (
     <>
       <header>
-        <GiHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+        <GiHamburgerMenu onClick={() => setShowNav(!showNav)} />
       </header>
-      <NavBar show={showNav}/>
-      <div className={`mainDiv ${showNav ? "active" : ""}`}> 
-      
+      <NavBar show={showNav && !hideNavBar} />
+      <div className={`mainDiv ${showNav && !hideNavBar ? "active" : ""}`}>
         <About />
-        <div className="content">
-          skills
-        </div>
-        <div className="content">
-          Proyects
-        </div>
+        <div className="content">Skills</div>
+        <div className="content">Projects</div>
         <div className="content">Sports</div>
       </div>
     </>
